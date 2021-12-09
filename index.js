5d94e4361038107fdd87ce24191d0b18bfe4442f
caio-gferreira <caiogfferreira@gmail.com>
2021-12-08 22:38:11 -0300

    adicionando mais efeitos sonoros e colocando um comando que lista todos os efeitos

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ client.on('message', async msg => {
 
   if (!msg.content.startsWith(prefixo)) return;
 
-  if (msg.content === prefixo + 'join') {
-      servers.server.connection = (await msg.member.voice.channel.join())
+  if (msg.content === prefixo + 'commands') {
+      msg.channel.send(`
+      #play badum - bateria;\n\n#play boom - explosão;\n\n#play clap - aplausos;\n\n#play cow - vaca;\n\n#play donkey - jumento;\n\n#play hehe - michael jackson;\n\n#play horse - cavalo;\n\n#play rusbe - michael jackson2;\n\n#play wee - weeeeee`)
     }
     const sound = msg.content.replace(/#play /gi, '')
     
